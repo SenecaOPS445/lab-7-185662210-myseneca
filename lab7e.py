501ce18ebfeb5c14725d30b39cb6901b2ae01ae7
Ched Duloy <185662210-myseneca>
2024-07-24 15:28:11 -0400

    finished lab7

--- lab7e.py
+++ lab7e.py
@@ -13,6 +13,15 @@ class Time:
         self.minute = minute
         self.second = second
 
+    def __str__(self):
+        '''return a string representation for the object self'''
+        return '%.2d:%.2d:%.2d' % (self.hour, self.minute, self.second)
+
+    def __repr__(self):
+        '''return a string representation for the object self'''
+        '''just instead of ':', you are required use the '.'  in the formatting string.'''
+        return '%.2d.%.2d.%.2d' % (self.hour, self.minute, self.second)
+
     def format_time(self):
         """Return time object (t) as a formatted string"""
         return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
